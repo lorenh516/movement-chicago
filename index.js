bb151c02961806642f8200d1332bf3a4efbbe983
Loren Hinkson <loren.hinkson@gmail.com>
2019-03-13 01:57:55 -0500

    new data + map popups

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   Promise.all([
       './data/Boundaries - Community Areas (current).geojson',
       './data/tractData.json',
-      './data/tractDataGeoms.geojson'
+      './data/tractDataGeomsComm.geojson'
     ].map(url => fetch(url).then(data => data.json())))
       .then(data => plotChi(data))
     .catch(function(error){
@@ -34,7 +34,7 @@ function plotChi(data) {
   const height = 400;
   const width = 500;
 
-  const margin = {top: 50, left: 75, right: 25, bottom: 25};
+  const margin = {top: 50, left: 75, right: 0, bottom: 25};
   const plotWidth = width - margin.left - margin.right;
   const plotHeight = height - margin.bottom - margin.top;
 
@@ -58,6 +58,7 @@ function plotChi(data) {
   // Initialize an svg for scatterplot
   var svg = d3.select('.chart-container')
     .append("svg")
+    .attr('id', 'chart-area')
     .attr('width', width)
     .attr('height', height);
 
@@ -97,12 +98,33 @@ function plotChi(data) {
     .domain([aPop.min, wPop.max])
     .range([2, 4, 6, 8]);
 
+  // define color scale
+  // const popColors = d3.scaleLinear()
+  //   .domain([incomeDomain.min, incomeDomain.max])
+  //   .range(['#326d3c','#467e4d','#5b905f','#72a170','#8ab280','#a7c28d','#edc876']);
+  console.log(incomeDomain.max / 9 >> 0);
+
+  const popColors = d3.scaleSequential(d3.interpolateYlOrBr).domain([incomeDomain.min, incomeDomain.max])
+
   // map populations to data properties
+  // const propertyMapping = {
+  //     Latinx: {population: 'latinxPop', percent: 'latinx_pct', color: '#6d7d53'},
+  //     Asian: {population: 'asianPop', percent: 'asian_pct', color: '#986769'},
+  //     Black: {population: 'latinxPop', percent: 'latinx_pct', color: '#6d7d53'},
+  //     White: {population: 'whitePop', percent: 'latinx_pct', color: '#30505a'},
   const propertyMapping = {
-      Latinx: 'latinxPop',
-      Black: 'blackPop',
-      Asian: 'asianPop',
-      White: 'whitePop'
+      Latinx: {pop: 'latinxPop', pct: 'latinx_pct'},
+      Black: {pop: 'blackPop', pct: 'black_pct'},
+      Asian: {pop: 'asianPop', pct: 'asian_pct'},
+      White: {pop: 'whitePop', pct: 'white_pct'}
+    }
+
+
+    const tractColors = {
+      "latinx": "#6d7d53",
+      "black": "#9d8e64",
+      "asian": "#986769",
+      "white": "#30505a"
     }
 
     // add y-axis title, which does not change
@@ -126,9 +148,11 @@ function plotChi(data) {
     svg.selectAll('.tick > text')
       .style('font-family', "Roboto");
 
+
+
   // MAKE STATE VARIABLE HERE (WHICH ONE IS SELECTED)
-  var checkedRadio = d3.select('input[name="map-filter-radio"]:checked').value;
-  console.log(checkedRadio);
+  // var checkedRadio = d3.select('input[name="map-filter-radio"]:checked').value;
+  // console.log(checkedRadio);
 
   // SAVE JOIN FOR INPUTS
   clean = d3.selectAll(".filter-radio")
@@ -149,6 +173,10 @@ function plotChi(data) {
     .append('label')
       .attr('for', d => d.group)
       .text(d => d.group)
+        .classed('predom-black', d => d.group === 'Black')
+        .classed('predom-asian', d => d.group === 'Asian')
+        .classed('predom-latinx', d => d.group === 'Latinx')
+        .classed('predom-white', d => d.group === 'White')
     .append('input')
       .attr('name', 'map-filter-radio')
       .attr('class', 'filter-radio')
@@ -158,7 +186,7 @@ function plotChi(data) {
       .merge(clean)
       .on('click', function(d) {
 
-      mapProperty = propertyMapping[d.group];
+      mapProperty = propertyMapping[d.group]['pop'];
       updateChart(plotGroup, svg, tractDetails, xScale, yScale, rScale, mapProperty, margin, plotHeight, plotWidth, currentYear);
       mapUpdate(currentYear, mapProperty);
     });
@@ -169,7 +197,7 @@ function plotChi(data) {
   updateChart(plotGroup, svg, tractDetails, xScale, yScale, rScale, mapProperty, margin, plotHeight, plotWidth, currentYear);
 
 
-  const mapUpdate = makeMap(tractData, communityShapes, tractDetails, rScale);
+  const mapUpdate = makeMap(tractData, communityShapes, tractDetails, rScale, popColors, propertyMapping);
   d3.select('.button-container')
     .selectAll('button')
     .data([2012, 2017])
@@ -190,17 +218,8 @@ function plotChi(data) {
 
 
 
-  function makeMap(tractData, communityShapes, tractDetails, rScale) {
+  function makeMap(tractData, communityShapes, tractDetails, rScale, popColors, propertyMapping) {
     // initiate scale domains
-    const wPop = computeDomain(tractData, 'whitePop');
-    const aPop = computeDomain(tractData, 'asianPop');
-
-    const incomeDomain = computeDomain(tractData, 'medianIncome');
-
-    // define color scale
-    const popColors = d3.scaleQuantize()
-      .domain([incomeDomain.min, incomeDomain.max])
-      .range(['#f6eff7','#d0d1e6','#a6bddb','#67a9cf','#3690c0','#02818a','#016450']);
 
     // initiate map object
     var mymap = L.map('map', {
@@ -213,7 +232,8 @@ function plotChi(data) {
     var aToken = 'pk.eyJ1IjoibGhpbmtzb24iLCJhIjoiY2pzcW52ZnoxMDFyejQ0cGRtZDRtMWppNSJ9.jIRwyua0hfpSpvwjxZcZkQ'; //public key
     var baseMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
+      maxZoom: 14,
+      minZoom: 10,
       id: 'mapbox.light',
       accessToken: aToken
   }).addTo(mymap);
@@ -235,7 +255,7 @@ function plotChi(data) {
     var centerpointlayer = new L.LayerGroup();
     var tracts = new L.LayerGroup();
 
-    return function updateMap(currentYear, property, radiusScale=rScale, colorScale=popColors, map=mymap) {
+    return function updateMap(currentYear, property, radiusScale=rScale, colorScale=popColors, map=mymap, colnames=propertyMapping) {
 
       const tractColors = {
         "latinx": "#6d7d53",
@@ -296,16 +316,35 @@ function plotChi(data) {
            var bounds = layer.getBounds();
            var center = bounds.getCenter();
            // console.log(feature)
-           L.circleMarker(center,{
+           tractMarker = L.circleMarker(center,{
            // L.circleMarker([feature.properties.lat, feature.properties.long],{
              radius: radiusScale(feature.properties[property]),
              color: colorScale(feature.properties.medianIncome),
              id: feature.properties.GEOID,
              weight: 0.75,
              opacity: 1,
-             fillOpacity: 0.5,
+             fillOpacity: 0.65,
              className: `${feature.properties.GEOID} map-dots`
            }).addTo(centerpointlayer);
+
+           // tractMarker.on('mouseover', function(e){
+           //   var mapTooltip = L.tooltip()
+           //   .setLatLng(e.latlng)
+           //   .setContent(`Population Group: ${feature.properties[property]}\nTotal Population: ${feature.properties.population}\nMedian Income: ${feature.properties.medianIncome}\nPercentage Non-White: ${1 - feature.properties.white_pct}`)
+           //   .openOn(map);
+           // });
+           tractMarker.bindPopup(`Total Population: ${feature.properties.population}\nGroup Size: ${feature.properties[property]} (${(100 * (feature.properties[property]/feature.properties.population)).toFixed(2)}%)\nMedian Income: ${feature.properties.medianIncome}\nMedian Income Change: ${feature.properties.fullPeriodChange}\nPct. Non-White: ${(100*(1 - feature.properties.white_pct)).toFixed(2)}%`,
+           {maxWidth: 150,
+           minWidth: 50,
+           maxHeight: 100,
+           closeButton: false,
+          autoPanPadding: L.point(2,2)});
+           tractMarker.on('mouseover', function (e) {
+               this.openPopup();
+           });
+           tractMarker.on('mouseout', function (e) {
+               this.closePopup();
+          });
          };
        }
 
@@ -328,34 +367,127 @@ function updateChart(plotGroup, svg, tractDetails, xScale, yScale, rScale, prope
       whitePop: 'White'
     };
 
+    // add tooltips
+    var tooltip = d3.select("#chart-area")
+      .append("div")
+      // created as transparent so visibility can be triggered by mouseover
+      .style("opacity", 0)
+      .attr("class", "tooltip");
+
+
+    // A function that change this tooltip when the user hover a point.
+    // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+    var mouseover = function(d) {
+      tooltip
+        .transition()
+        .duration(200)
+        // mouseover ==> can see tooltip
+        .style("opacity", 0.75);
+        // .html(`GEOID: ${d.properties.GEOID}`)
+        // .style("left", `${d3.event.pageX}px`)
+        // .style("top", `${d3.event.pageY}px`);
+    }
+
+    var mousemove = function(d) {
+      // console.log(`${d3.mouse(this)[0]}`)
+      tooltip
+        .html(`GEOID: ${d.properties.GEOID}`)
+        // .style("left", `${d3.mouse(this)[0]}px`)
+        // .style('left', `${d3.event.pageX}px`)
+        // .style("top", `${d3.event.pageY}px`);
+        // .style('left', '0px')
+        // .style("top", `0px`);
+        .style('left', `${d3.mouse(this)[0]}`)
+        .style("top", `${d3.mouse(this)[1]}`);
+    }
+
+    // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+    var mouseleave = function(d) {
+      tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", 0)
+    }
+
   const scattered = plotGroup.selectAll('.circle')
     // data filtering adapted from example at bl.ocks.org:
     // https://bl.ocks.org/fabiomainardi/00fd581dc5ba92d99eec
-    .data(tractDetails.features.filter( d => d.properties.id == currentYear));
+    .data(tractDetails.features.filter( d => d.properties.id === currentYear),
+          d => `${d.properties.GEOID}-${d.properties.id}`);
+    // .data(tractDetails.features.filter( d => (d.properties.id === currentYear && d.properties[property] > 0)),
+    //       d => d);
+    // .data(tractDetails.features.filter( d => (d.properties.id === currentYear && d.properties[property] > 0)),
+    //       d => `${d.properties.GEOID}-${d.properties.id}`);
+
+    // idea for adding multiple classes to same selection from benclinkinbeard.com
+    // https://benclinkinbeard.com/d3tips/attrclass-vs-classed/
+
 
   scattered.enter()
     .append('circle')
-    .merge(scattered)
     .attr('class', 'circle')
-    .attr('cx', d => xScale(d.properties.whitePop))
+    .merge(scattered)
+    .attr('cx', d => xScale(d.properties[property]))
     .attr('cy', d => yScale(d.properties.medianIncome))
-    // .attr('transform', `translate(${margin.left}, ${margin.top})`)
-    .attr('r', d => rScale(d.properties[property]))
+    .attr('r', d => (Number(d.properties[property]) === 0) ? 0:rScale(d.properties[property]))
     .attr('id', d => d.properties.GEOID)
-    .attr('class', function(d) {
-      if (d.properties.predominant_race == 'Black') {
-        return 'predom-black'}
-      else if (d.properties.predominant_race == 'Latinx') {
-        return 'predom-latinx'}
-      else if (d.properties.predominant_race == 'Asian') {
-        return 'predom-asian'}
-      else {return 'predom-white'}
-    })
-    // idea for adding multiple classes to same selection from benclinkinbeard.com
-    // https://benclinkinbeard.com/d3tips/attrclass-vs-classed/
-    .classed('circle', d => d.GEOID === d.GEOID)
+      .classed('predom-black', d => d.properties.predominant_race === 'Black')
+    .classed('predom-latinx', d => d.properties.predominant_race === 'Latinx')
+    .classed('predom-asian', d => d.properties.predominant_race === 'Asian')
+    .classed('predom-white', d => d.properties.predominant_race === 'White')
     .classed('circle-increased', d => d.properties.fullPeriodChange > 0)
-    .classed('circle-decreased', d => d.properties.fullPeriodChange < 0);
+    .classed('circle-decreased', d => d.properties.fullPeriodChange < 0)
+    .on("mouseenter", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave);
+    // .attr('cx', d => xScale(d.properties.whitePop))
+
+    // scattered
+    //   .attr('class', 'circle')
+    //   .transition().duration(1000)
+    //   .attr('cx', d => xScale(d.properties[property]))
+    //   // .attr('cx', d => xScale(d.properties.whitePop))
+    //   .attr('cy', d => yScale(d.properties.medianIncome))
+    //   .attr('r', d => rScale(d.properties[property]))
+    //   .attr('id', d => d.properties.GEOID)
+    //   // idea for adding multiple classes to same selection from benclinkinbeard.com
+    //   // https://benclinkinbeard.com/d3tips/attrclass-vs-classed/
+    //   .classed('predom-black', d => d.properties.predominant_race === 'Black')
+    //   .classed('predom-latinx', d => d.properties.predominant_race === 'Latinx')
+    //   .classed('predom-asian', d => d.properties.predominant_race === 'Asian')
+    //   .classed('predom-white', d => d.properties.predominant_race === 'White')
+    //   .classed('circle-increased', d => d.properties.fullPeriodChange > 0)
+    //   .classed('circle-decreased', d => d.properties.fullPeriodChange < 0)
+    //   .classed('circle-increased', d => d.properties.fullPeriodChange > 0)
+    //   .classed('circle-decreased', d => d.properties.fullPeriodChange < 0)
+    //   .on("mouseenter", mouseover)
+    //   .on("mousemove", mousemove)
+    //   .on("mouseleave", mouseleave);
+
+    //
+    //   scattered.exit().transition('fill', 'red').duration(200).remove();
+
+  // scattered
+  //   .attr('class', 'circle')
+  //   .transition().duration(1000)
+  //   .attr('cx', d => xScale(d.properties[property]))
+  //   // .attr('cx', d => xScale(d.properties.whitePop))
+  //   .attr('cy', d => yScale(d.properties.medianIncome))
+  //   .attr('r', d => rScale(d.properties[property]))
+  //   .attr('id', d => d.properties.GEOID)
+  //   // idea for adding multiple classes to same selection from benclinkinbeard.com
+  //   // https://benclinkinbeard.com/d3tips/attrclass-vs-classed/
+  //   .classed('predom-black', d => d.properties.predominant_race === 'Black')
+  //   .classed('predom-latinx', d => d.properties.predominant_race === 'Latinx')
+  //   .classed('predom-asian', d => d.properties.predominant_race === 'Asian')
+  //   .classed('predom-white', d => d.properties.predominant_race === 'White')
+  //   .classed('circle-increased', d => d.properties.fullPeriodChange > 0)
+  //   .classed('circle-decreased', d => d.properties.fullPeriodChange < 0)
+  //   .on("mouseover", mouseover)
+  //   .on("mousemove", mousemove)
+  //   .on("mouseleave", mouseleave);
+  //
+
 
     // adding x-axis title
     const xlabel = svg.selectAll('.xaxis')
@@ -371,6 +503,6 @@ function updateChart(plotGroup, svg, tractDetails, xScale, yScale, rScale, prope
       .attr('x', d => d.xlabelx)
       .attr('y', d => d.xlabely)
       .attr('text-anchor', d=> d.anchor)
-      .text(`${populationMapping[property]} Population Percentage of Census Tract`);
+      .text(`${populationMapping[property]} Population in Census Tract`);
 
 };
